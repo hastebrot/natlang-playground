11c92ce75e388b882beff1505e1300a275b21edb
Benjamin Gudehus <hastebrot@gmail.com>
2019-03-21 00:02:05 +0100

    (feature) Add processParagraphs() helper function.

--- packages/natlang-ui/src/components/textbox.js
+++ packages/natlang-ui/src/components/textbox.js
@@ -2,15 +2,9 @@ import React, { Fragment, useState } from "react"
 import { Pane, Text } from "fannypack"
 import { produce } from "immer"
 
-export default () => {
-  const paragraphs =
-    "Le rielaborazioni non sono vietate. Ma chi pretende di fare la vera carbonara deve usare solo queste materie: la tradizione è un ingrediente come gli altri."
-
-  const _words = paragraphs
-    .split(" ")
-    .map((text, index) => ({ index, text, style: {} }))
-
-  const [words, setWords] = useState(_words)
+export default ({ paragraphs }) => {
+  const data = processParagraphs(paragraphs)
+  const [words, setWords] = useState(data[0].words)
 
   return (
     <Pane padding="major-1" backgroundColor="white700">
@@ -34,4 +28,32 @@ export default () => {
       ))}
     </Pane>
   )
+}
+
+export const processParagraphs = paragraphsText => {
+  const paragraphs = splitParagraphs(paragraphsText)
+  const paragraphsData = paragraphs.map((paragraphText, index) => {
+    const words = splitWords(paragraphText)
+    const wordsData = words.map((wordText, index) => ({
+      type: "word",
+      index,
+      text: wordText,
+      style: {},
+    }))
+    return {
+      type: "paragraph",
+      index,
+      text: paragraphText,
+      words: wordsData,
+    }
+  })
+  return paragraphsData
+}
+
+export const splitParagraphs = text => {
+  return text.trim().split(/\n+/)
+}
+
+export const splitWords = text => {
+  return text.split(/\s+/)
 }
--- packages/natlang-ui/src/stories/textbox.story.js
+++ packages/natlang-ui/src/stories/textbox.story.js
@@ -9,4 +9,14 @@ stories.addDecorator(story => (
     <Pane padding="major-2">{story()}</Pane>
   </ThemeProvider>
 ))
-stories.add("default", () => <Textbox />)
+stories.add("default", () => <Textbox paragraphs={paragraphs} />)
+
+const paragraphs = `
+Ils formaient une assemblée bien grotesque ces êtres singuliers réunis sur le bord de la mare ; les uns avaient leurs plumes tout en désordre, les autres le poil plaqué contre le corps. Tous étaient trempés, de mauvaise humeur, et fort mal à l’aise.
+
+« Comment faire pour nous sécher ? » ce fut la première question, cela va sans dire. Au bout de quelques instants, il sembla tout naturel à Alice de causer familièrement avec ces animaux, comme si elle les connaissait depuis son berceau. Elle eut même une longue discussion avec le Lory, qui, à la fin, lui fit la mine et lui dit d’un air boudeur : « Je suis plus âgé que vous, et je dois par conséquent en savoir plus long. » Alice ne voulut pas accepter cette conclusion avant de savoir l’âge du Lory, et comme celui-ci refusa tout net de le lui dire, cela mit un terme au débat.
+
+Enfin la Souris, qui paraissait avoir un certain ascendant sur les autres, leur cria : « Asseyez-vous tous, et écoutez-moi ! Je vais bientôt vous faire sécher, je vous en réponds ! » Vite, tout le monde s’assit en rond autour de la Souris, sur qui Alice tenait les yeux fixés avec inquiétude, car elle se disait : « Je vais attraper un vilain rhume si je ne sèche pas bientôt. »
+
+« Hum ! » fit la Souris d’un air d’importance ; « êtes-vous prêts ? Je ne sais rien de plus sec que ceci. Silence dans le cercle, je vous prie. « Guillaume le Conquérant, dont le pape avait embrassé le parti, soumit bientôt les Anglais, qui manquaient de chefs, et commençaient à s’accoutumer aux usurpations et aux conquêtes des étrangers. Edwin et Morcar, comtes de Mercie et de Northumbrie — » »
+`
