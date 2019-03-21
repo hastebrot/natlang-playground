a8de753f89fda5ef8f06af070a393ce29ef2d66f
Benjamin Gudehus <hastebrot@gmail.com>
2019-03-21 02:39:50 +0100

    (doc) Build: Update docs files.

---
+++ build/natlang-ui-docs/precache-manifest.362acad28dc864b1cec93877acc69195.js
@@ -0,0 +1,15 @@
+self.__precacheManifest = [
+  {
+    "url": "vendors~main.d2dd2ae8f186e91edcda.bundle.js"
+  },
+  {
+    "url": "runtime~main.d2dd2ae8f186e91edcda.bundle.js"
+  },
+  {
+    "url": "main.d2dd2ae8f186e91edcda.bundle.js"
+  },
+  {
+    "revision": "64e8835ba362cbc9657cd23afd796b18",
+    "url": "iframe.html"
+  }
+];
